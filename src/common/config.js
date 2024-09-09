973910c078c75baf3323f2ab0a3e90ec64ff29d0
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 20:18:43 +0300

    add config,ts

--- src/common/config.js
+++ src/common/config.js
@@ -1,18 +1,18 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
 
-// environment
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const { NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, PORT, AUTH_MODE } = process.env;
 
-// application
-const PORT = +process.env.PORT || 4000;
+const config = {
+  NODE_ENV,
+  PORT: PORT ?? 4000,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+  AUTH_MODE: AUTH_MODE === 'true',
+};
 
-// JWT
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret-key';
-
-// database
-const MONGO_CONNECTION_STRING =
-  process.env.MONGO_CONNECTION_STRING || 'your-mongo-db-connection-string';
-
-export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY };
+export default config;
